7c61322d1354f63601eec9ae1f8c7cf9fe850338
Aseem Jain <jainaseem2004@gmail.com>
2024-02-06 00:11:03 +0530

    Day 3 - Arrays

--- Day 3/Arrays.js
+++ Day 3/Arrays.js
@@ -22,11 +22,11 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-/**
+/*
 *   Return the second largest number in the array.
 *   @param {Number[]} nums - An array of numbers.
 *   @return {Number} The second largest number in the array.
-**/
+*/
 function getSecondLargest(nums) {
     // Complete the function
     const max=Math.max(...nums);
